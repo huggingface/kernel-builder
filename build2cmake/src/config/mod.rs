f83be20bbd5a51984dc7ef0188441464cd295b73
Daniël de Kok <me@danieldk.eu>
2025-05-27 17:02:22 +0000

    Update the build.toml format in preparation for Metal
    Changes:
    
    * `kernel.<name>.language` is removed.
    * `kernel.<name>.backend` is added and is one of `cuda` or `rocm`.
    * `torch.universal` is renamed to `general.universal`.
    * `.cu` files in kernels with the `rocm` backend are hipified.
    
    `build2toml` still parses the old format and adds a new `update-build`
    subcommand to upgrade a `build.toml`. The Nix tooling does not support
    the old format, but will suggest to run `build2toml update-build`.

---
+++ build2cmake/src/config/mod.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+
+pub mod v1;
+
+mod v2;
+pub use v2::{Backend, Build, Dependencies, Kernel, Torch};
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum BuildCompat {
+    V1(v1::Build),
+    V2(Build),
+}
+
+impl From<BuildCompat> for Build {
+    fn from(compat: BuildCompat) -> Self {
+        match compat {
+            BuildCompat::V1(v1_build) => v1_build.into(),
+            BuildCompat::V2(v2_build) => v2_build,
+        }
+    }
+}
