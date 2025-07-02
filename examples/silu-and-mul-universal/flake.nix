7068bae65f1aea82edecaa02f6885d9e537f6576
osbm <osmanfbayram@gmail.com>
2025-07-02 18:39:45 +0300

    add huggingface and nix-community nix caches to flakes

--- examples/silu-and-mul-universal/flake.nix
+++ examples/silu-and-mul-universal/flake.nix
@@ -1,6 +1,17 @@
 {
   description = "Flake for kernels tests";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://huggingface.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "huggingface.cachix.org-1:ynTPbLS0W8ofXd9fDjk1KvoFky9K2jhxe6r4nXAkc/o="
+    ];
+  };
+
   inputs = {
     kernel-builder.url = "path:../..";
   };
--- kernel-abi-check/flake.nix
+++ kernel-abi-check/flake.nix
@@ -1,6 +1,17 @@
 {
   description = "kernel-abi-check devenv";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://huggingface.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "huggingface.cachix.org-1:ynTPbLS0W8ofXd9fDjk1KvoFky9K2jhxe6r4nXAkc/o="
+    ];
+  };
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
