bca2f448146231955f11b448fe9861ae304dcc1f
Daniël de Kok <me@danieldk.eu>
2025-09-30 10:39:59 +0000

    ROCm: apply specified archs to sources

--- build2cmake/src/templates/cuda/kernel.cmake
+++ build2cmake/src/templates/cuda/kernel.cmake
@@ -49,7 +49,19 @@ if(GPU_LANG STREQUAL "CUDA")
   list(APPEND SRC {{'"${' + kernel_name + '_SRC}"'}})
 {% if supports_hipify %}
 elseif(GPU_LANG STREQUAL "HIP")
-  hip_archs_loose_intersection({{kernel_name}}_ARCHS "{{ rocm_archs|join(";") }}" ${ROCM_ARCHS})
+  hip_archs_loose_intersection({{kernel_name}}_ARCHS "{{ rocm_archs|join(";") }}" "${ROCM_ARCHS}")
+  message(STATUS "Archs for kernel {{kernel_name}}: {{ '${' + kernel_name + '_ARCHS}'}}")
+
+  foreach(_KERNEL_SRC {{'${' + kernel_name + '_SRC}'}})
+    if(_KERNEL_SRC MATCHES ".*\\.hip$")
+      set_property(
+        SOURCE ${_KERNEL_SRC}
+        PROPERTY
+        HIP_ARCHITECTURES {{ '${' + kernel_name + '_ARCHS}'}}
+      )
+    endif()
+  endforeach()
+
   list(APPEND SRC {{'"${' + kernel_name + '_SRC}"'}})
 {% endif %}
 endif()
