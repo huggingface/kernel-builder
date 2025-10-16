25afb1e329b2e710c24c00a6c9a3991499fc30aa
Daniël de Kok <me@danieldk.eu>
2025-10-16 10:29:53 +0000

    Use Torch 2.9 from binary wheels, remove Torch 2.7

--- versions.nix
+++ versions.nix
@@ -1,53 +1,5 @@
 [
   {
-    torchVersion = "2.7";
-    cudaVersion = "11.8";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    cudaVersion = "12.6";
-    cxx11Abi = true;
-    systems = [
-      "x86_64-linux"
-    ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    cudaVersion = "12.8";
-    cxx11Abi = true;
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    rocmVersion = "6.3.4";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    xpuVersion = "2025.0.2";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    cxx11Abi = true;
-    metal = true;
-    systems = [ "aarch64-darwin" ];
-    bundleBuild = true;
-  }
-
-  {
     torchVersion = "2.8";
     xpuVersion = "2025.1.3";
     cxx11Abi = true;
@@ -109,7 +61,6 @@
     cxx11Abi = true;
     systems = [ "x86_64-linux" ];
     bundleBuild = true;
-    sourceBuild = true;
   }
   {
     torchVersion = "2.9";
@@ -120,7 +71,6 @@
       "aarch64-linux"
     ];
     bundleBuild = true;
-    sourceBuild = true;
   }
   {
     torchVersion = "2.9";
@@ -131,7 +81,6 @@
       "aarch64-linux"
     ];
     bundleBuild = true;
-    sourceBuild = true;
   }
   {
     torchVersion = "2.9";
@@ -142,7 +91,6 @@
       "aarch64-linux"
     ];
     bundleBuild = true;
-    sourceBuild = true;
   }
   {
     torchVersion = "2.9";
