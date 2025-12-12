a4eb89114a27e68fdecf375655aa2fc1ead50e0c
medmekk <mohamed.mekkouri@huggingface.co>
2025-12-12 05:28:13 +0000

    add to testShells

--- lib/build.nix
+++ lib/build.nix
@@ -263,6 +263,7 @@ rec {
             ];
             shellHook = ''
               export PYTHONPATH=''${PYTHONPATH}:${extension}
+              unset LD_LIBRARY_PATH
             '';
           };
         };
