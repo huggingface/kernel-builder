8d35e7246070841d59a0b328f2aba3f4e0b168eb
Daniël de Kok <me@danieldk.eu>
2026-01-05 12:27:01 +0000

    Fix CUDA `backendStdenv` use
    In nixpkgs `cudaPackages.backendStdenv` is not a wrapper anymore and
    calling `override` applies it to what used to be the underlying stdenv.
    Adjust our use of `backendStdenv` to fix this. Unfortunately, this also
    entails vendoring `backendStdenv`, since we cannot override it anymore.

--- overlay.nix
+++ overlay.nix
@@ -45,7 +45,22 @@ in
 
   remove-bytecode-hook = prev.callPackage ./pkgs/remove-bytecode-hook { };
 
-  stdenvGlibc_2_27 = prev.callPackage ./pkgs/stdenv-glibc-2_27 { };
+  stdenvGlibc_2_27 = import ./pkgs/stdenv-glibc-2_27 {
+    # Do not use callPackage, because we want overrides to apply to
+    # the stdenv itself and not this file.
+    inherit (final)
+      config
+      fetchFromGitHub
+      overrideCC
+      wrapBintoolsWith
+      wrapCCWith
+      gcc13Stdenv
+      stdenv
+      bintools-unwrapped
+      cudaPackages
+      libgcc
+      ;
+  };
 
   ucx = prev.ucx.overrideAttrs (
     _: prevAttrs: {
